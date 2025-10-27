66837ea659e4309deb22cf922e420cb7c7d29b25
Bazlur Rashid <mailtobazlur@gmail.com>
2025-10-27 18:22:28 +0600

    admin controllers created

---
+++ app/controllers/admin/portfolio_managers_controller.rb
@@ -0,0 +1,33 @@
+module Admin
+  class PortfolioManagersController < ApplicationController
+    layout 'admin'
+    
+    # GET /admin/portfolio_managers
+    def index
+    end
+    
+    # GET /admin/portfolio_managers/:id
+    def show
+    end
+    
+    # GET /admin/portfolio_managers/new
+    def new
+    end
+    
+    # POST /admin/portfolio_managers
+    def create
+    end
+    
+    # GET /admin/portfolio_managers/:id/edit
+    def edit
+    end
+    
+    # PATCH/PUT /admin/portfolio_managers/:id
+    def update
+    end
+    
+    # DELETE /admin/portfolio_managers/:id
+    def destroy
+    end
+  end
+end
---
+++ app/controllers/admin/price_references_controller.rb
@@ -0,0 +1,45 @@
+module Admin
+  class PriceReferencesController < ApplicationController
+    layout 'admin'
+    
+    # GET /admin/price_references
+    def index
+    end
+    
+    # GET /admin/price_references/:id
+    def show
+    end
+    
+    # GET /admin/price_references/new
+    def new
+    end
+    
+    # POST /admin/price_references
+    def create
+    end
+    
+    # GET /admin/price_references/:id/edit
+    def edit
+    end
+    
+    # PATCH/PUT /admin/price_references/:id
+    def update
+    end
+    
+    # DELETE /admin/price_references/:id
+    def destroy
+    end
+    
+    # GET /admin/price_references/import
+    def import
+    end
+    
+    # POST /admin/price_references/import
+    def process_import
+    end
+    
+    # GET /admin/price_references/export
+    def export
+    end
+  end
+end
