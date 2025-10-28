4c2d222d7725da192c15ae79d762de2fbbbff021
Bazlur Rashid <mailtobazlur@gmail.com>
2025-10-28 22:05:05 +0600

    - spaces ui updated

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -6,7 +6,12 @@ class MockModel
   include ActiveModel::Model
   include ActiveModel::Conversion
   
-  attr_accessor :id, :name, :space_type, :area, :capacity, :description, :level_id
+  attr_accessor :id, :name, :space_type, :area, :capacity, :description, :level_id,
+                :ceiling_height, :primary_use, :secondary_use, :floor_covering, 
+                :wall_covering, :ceiling_type, :present_equipment, :water_points,
+                :electrical_outlets, :natural_lighting, :ventilation, :pmr_accessibility,
+                :has_windows, :network_connectivity, :heating, :air_conditioning,
+                :omniclass_code, :grouping_zone, :notes
   
   def initialize(attributes = {})
     attributes.each do |key, value|
@@ -37,7 +42,7 @@ class SpacesController < ApplicationController
     @level = OpenStruct.new(id: params[:level_id], name: "Rez-de-chaussée")
     @spaces = [
       OpenStruct.new(
-        id: 'space-101',
+        id: 1,
         name: 'Bureau 101',
         type: 'Bureau',
         area: 25,
@@ -45,7 +50,7 @@ class SpacesController < ApplicationController
         equipment_count: 3
       ),
       OpenStruct.new(
-        id: 'space-102',
+        id: 2,
         name: 'Bureau 102',
         type: 'Bureau',
         area: 25,
@@ -53,7 +58,7 @@ class SpacesController < ApplicationController
         equipment_count: 3
       ),
       OpenStruct.new(
-        id: 'space-a',
+        id: 3,
         name: 'Salle de réunion A',
         type: 'Salle de réunion',
         area: 40,
@@ -92,6 +97,24 @@ class SpacesController < ApplicationController
   end
 
   def edit
+    # Use MockModel for the space
+    @space = MockModel.new(
+      id: params[:id],
+      name: "Bureau 101",
+      space_type: "office",
+      area: 25,
+      capacity: 2,
+      description: "Bureau standard"
+    )
+    def @space.model_name
+      ActiveModel::Name.new(self.class, nil, "Space")
+    end
+    
+    # Mock level
+    @level = MockModel.new(id: 1, name: "Rez-de-chaussée")
+    def @level.model_name
+      ActiveModel::Name.new(self.class, nil, "Level")
+    end
     # Renders spaces/edit.html.erb
   end
 
