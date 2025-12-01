330fd8f64632a42a026ccfbec29889ce4362ce09
Bazlur Rashid <mailtobazlur@gmail.com>
2025-12-01 22:48:22 +0600

    fix: Improve log viewer readability with white on black
    - Changed log text color from color-coded to pure white
    - Changed background from dark gray to pure black
    - Removed color legend (now using simple white text)
    - Added display info note explaining the color scheme
    - Improved contrast for better readability

--- app/helpers/admin/logs_helper.rb
+++ app/helpers/admin/logs_helper.rb
@@ -1,17 +1,6 @@
 module Admin::LogsHelper
   def format_log_line(line)
-    # Color code based on log level
-    case line
-    when /ERROR|FATAL/
-      "<span class='text-red-400'>#{ERB::Util.html_escape(line)}</span>".html_safe
-    when /WARN/
-      "<span class='text-yellow-400'>#{ERB::Util.html_escape(line)}</span>".html_safe
-    when /INFO/
-      "<span class='text-blue-400'>#{ERB::Util.html_escape(line)}</span>".html_safe
-    when /DEBUG/
-      "<span class='text-gray-400'>#{ERB::Util.html_escape(line)}</span>".html_safe
-    else
-      "<span class='text-gray-300'>#{ERB::Util.html_escape(line)}</span>".html_safe
-    end
+    # Use white text for all log lines for better readability
+    "<span class='text-white'>#{ERB::Util.html_escape(line)}</span>".html_safe
   end
 end
